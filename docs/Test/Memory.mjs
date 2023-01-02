f92d49729e956cdffbadc957562870385630ed81
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2023-01-02 11:24:10 -0500

    Update Memory.mjs

--- docs/Test/Memory.mjs
+++ docs/Test/Memory.mjs
@@ -1,5 +1,5 @@
 /*
-(c) 2022 Scot Watson  All Rights Reserved
+(c) 2023 Scot Watson  All Rights Reserved
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
@@ -627,6 +627,7 @@ export class DataArray {
         throw "Invalid Argument";
       }
       const byteOffset = this.#ElementClass.BYTE_LENGTH * index;
+      console.log(this.#memoryView);
       const slice = this.#memoryView.createSlice({
         byteOffset: byteOffset,
         byteLength: this.#ElementClass.BYTE_LENGTH,
